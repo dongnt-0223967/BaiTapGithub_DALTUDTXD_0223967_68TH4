cd1841ca3a3d3523d6086f04ca2d2701a7ef870e
nvthong05-hash <nvthong05@gmail.com>
2026-01-30 01:24:27 +0700

    thongnee

--- BaiTapGithub_DALTUDTXD_0223967_68TH4/BaiTapGithub_DALTUDTXD_0223967_68TH4/MVVM/Viewmodels/LoginViewModel.cs
+++ BaiTapGithub_DALTUDTXD_0223967_68TH4/BaiTapGithub_DALTUDTXD_0223967_68TH4/MVVM/Viewmodels/LoginViewModel.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace BaiTapGithub_DALTUDTXD_0223967_68TH4.MVVM.Viewmodels
 {
     internal class LoginViewModel
     {
+        public ICommand LoginCommand => new RoutedCommand();
     }
 }
